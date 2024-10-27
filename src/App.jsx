40150b969a7765fec96a292f7d73922cd143a45e
JulioCSSousa <julio.sousa1993@gmail.com>
2024-10-27 09:57:34 -0300

    func delete no cart

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@
 
 import Provider from './context/Provider';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 export default function App() {
   return (
     <>
     <Provider>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Home />
     </Provider>
     
     </>
