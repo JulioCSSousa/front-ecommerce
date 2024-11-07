77cb0101d04d8193afe85dc51c7b5e1bfe4f284d
JulioCSSousa <julio.sousa1993@gmail.com>
2024-11-07 18:03:34 -0300

    consertando remoção carrinho

--- src/components/utils/ShopCart.jsx
+++ src/components/utils/ShopCart.jsx
@@ -1,23 +1,17 @@
-import './ShopCart.css'
+import './ShopCart.css';
 import { BsFillCartDashFill } from "react-icons/bs";
 import PropTypes from 'prop-types';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { AppContext } from '../../context/appContext';
 
 export default function CartItem({ data }) {
-    // eslint-disable-next-line react/prop-types
     const { image, name, price, quantity } = data;
     const { setCartItems } = useContext(AppContext);
-    const [setIsAdded] = useState(false);
-
 
     const handleRemoveCart = () => {
         setCartItems(prevItems => {
             const itemExists = prevItems.some(item => item.name === data.name);
             if (itemExists) {
-                setIsAdded(true);
-                setTimeout(() => setIsAdded(false), 2000);
-
                 return prevItems
                     .map(item =>
                         item.name === data.name
@@ -34,16 +28,16 @@ export default function CartItem({ data }) {
         <section>
             <section className="cart-item">
                 <div className="image-content">
-                    <img src={image} alt={name} className='cart-item-image' />
+                    <img src={image} alt={name} className="cart-item-image" />
                 </div>
                 <div className="span-content">
-                    <span className='span-num'>
+                    <span className="span-num">
                         {quantity}
                     </span>
                 </div>
                 <div className="cart-content">
                     <h3 className="cart-title">{name}</h3>
-                    <h3 className="cart-item-price">R$ {quantity * price || 1}</h3>
+                    <h3 className="cart-item-price">R$ {(quantity * price).toFixed(2)}</h3>
                 </div>
                 <div className="remove-icon-content">
                     <button type="button" className="remove-icon" onClick={handleRemoveCart}>
@@ -59,6 +53,7 @@ CartItem.propTypes = {
     data: PropTypes.shape({
         name: PropTypes.string.isRequired,
         image: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired
+        price: PropTypes.number.isRequired,
+        quantity: PropTypes.number.isRequired
     }).isRequired,
 };
